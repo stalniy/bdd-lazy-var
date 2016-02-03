39c53b615aa4cf836787698048d5f7c8fdc00d1e
Sergii Stotskyi <sergiy.stotskiy@gmail.com>
2016-02-03 09:57:47 +0000

    fix(test): remove odd test

--- spec/getter_defs_spec.js
+++ spec/getter_defs_spec.js
@@ -7,26 +7,6 @@ describe('Lazy vars defined as getter on "get" function', function() {
     return {};
   })
 
-  describe.skip('test me', function() {
-    subject(function() {
-      return [];
-    });
-
-    it('is empty', function() {
-      expect(get.subject).to.be.empty;
-    })
-
-    describe.skip('opa', function() {
-      subject(function() {
-        return 1;
-      });
-
-      it('bla', function() {
-        expect(get.subject).to.equal(1);
-      })
-    })
-  })
-
   describe('by default', function() {
     subject(function() {
       return {};
