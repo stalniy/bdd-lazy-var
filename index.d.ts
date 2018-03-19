78ece1eabaa1a15b42255448bfb92c2079e6ed5d
Sergii Stotskyi <sergiy.stotskiy@gmail.com>
2018-03-19 14:08:37 +0200

    feat(package): adds typescript declarations
    Relates to #40

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+export interface GetLazyVar {
+  (name: string): any;
+  [name: string]: any;
+}
+
+export const get: GetLazyVar;
+export function def(name: string, implementation: () => any): void;
+export function sharedExamplesFor(summary: string, implementation: () => void): void;
+export function itBehavesLike(summary: string, ...vars: any[]): void;
+export function includeExamplesFor(summary: string, ...vars: any[]): void;
+
+declare global {
+  interface Global {
+    const get: GetLazyVar;
+    function def(name: string, implementation: () => any): void;
+    function sharedExamplesFor(summary: string, implementation: () => void): void;
+    function itBehavesLike(summary: string, ...vars: any[]): void;
+    function includeExamplesFor(summary: string, ...vars: any[]): void;
+    [key: string]: any;
+  }
+}
