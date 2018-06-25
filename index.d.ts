9972146ebb34c7bf7ce7c9ac20517ffd9fabe335
Sergii <sergiy.stotskiy@gmail.com>
2018-06-25 18:20:17 +0300

    fix(ts): adds vars into `sharedExamplesFor` implementation

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,7 @@ interface GetLazyVar {
 
 export const get: GetLazyVar;
 export function def(name: string, implementation: () => any): void;
-export function sharedExamplesFor(summary: string, implementation: () => void): void;
+export function sharedExamplesFor(summary: string, implementation: (...vars: any[]) => void): void;
 export function itBehavesLike(summary: string, ...vars: any[]): void;
 export function includeExamplesFor(summary: string, ...vars: any[]): void;
 
