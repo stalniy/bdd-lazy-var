28563976d92fcb52cdca7351516d9d02b0da2ada
Sergii <sergiy.stotskiy@gmail.com>
2018-07-26 09:48:29 +0300

    revert(interface): reverts typescript changes for new `it` & `its` DSL

--- interface.d.ts
+++ interface.d.ts
@@ -2,16 +2,8 @@ interface GetLazyVar {
   (name: string): any;
 }
 
-interface Is {
-  expected: ReturnType<typeof global.expect>
-}
-
-export const is: Is;
 export const get: GetLazyVar;
 export function def(name: string, implementation: () => any): void;
 export function sharedExamplesFor(summary: string, implementation: (...vars: any[]) => void): void;
 export function itBehavesLike(summary: string, ...vars: any[]): void;
 export function includeExamplesFor(summary: string, ...vars: any[]): void;
-export const its: typeof global.it;
-export const xits: typeof gloval.it;
-export const fits: typeof gloval.it;
