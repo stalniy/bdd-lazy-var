e3ea5dfcf9c0cd23262a8fef31a15cff6c250afd
Dan <dan@monday.com>
2021-07-13 00:11:03 +0300

    Added TypeScript annotations

--- interface.d.ts
+++ interface.d.ts
@@ -4,8 +4,13 @@ interface GetLazyVar {
 
 type AnyFunction = (...args: any[]) => any;
 
+interface DefOptions {
+    lazy?: boolean;
+}
+
 export const get: GetLazyVar;
 export function def(name: string, implementation: () => any): void;
+export function def(name: string, implementation: () => any, defOptions: DefOptions): void;
 export function sharedExamplesFor(summary: string, implementation: (...vars: any[]) => void): void;
 export function itBehavesLike(summary: string | AnyFunction, ...vars: any[]): void;
 export function includeExamplesFor(summary: string | AnyFunction, ...vars: any[]): void;
