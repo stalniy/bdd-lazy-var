554ce72258d31354a675d792f6f001b3804fe1f4
Sergii <sergiy.stotskiy@gmail.com>
2026-01-25 08:43:53 +0100

    chore: remove obsolete types

--- getter.d.ts
+++
@@ -1,8 +0,0 @@
-declare module './interface' {
-  interface GetLazyVar {
-    (name: string): any;
-    [name: string]: any;
-  }
-}
-
-export * from './index'
--- global.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './index'
-
-// TODO: declare `$<varName>` globally
